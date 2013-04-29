25a27f92194d5b10241cc91630bf9a6ee3be327f
unknown <alexander.hanel@gmail.com>
2013-04-28 21:14:00 -0600

    Version 0.3 added

--- fwrapper.py
+++ fwrapper.py
@@ -1,8 +1,9 @@
 # Name: 
 #    fwrapper.py
 # Version: 
-#    0.2
-        # removed static instance name 
+#    0.3
+        # 0.2 - removed static instance name
+	# 0.3 - added usage function and add the option to pass a buffer to the patch function.
 # Description: 
 #    This script can be used to carve out data and work with data in IDA.           
 # Author
@@ -19,13 +20,41 @@ class fwrapper():
         self.ogLen = None
         self.status = True
         self.run()
+
+    def usage(self):
+	print '''
+	example:
+	select data then execute the below command. The data will be automatically populated. It 
+	should be noted that the whole instruction line will be added to the buffer. If the whole
+	line is not intended the obj.start or obj.end will need to be manually changed. 
+
+	obj = fwrapper()	 
+	for x in obj.buffer: print hex(ord(x)) // will print each item in thebuffer			
+
+	- data -
+	obj.start               contains the address of the start of the selected instruction.
+	obj.end                 contains the address of the end of the selected instruction. 
+	obj.buffer              contains the binary data.   
+	obj.ogLen               contains the size of the buffer. 
+
+	- function -
+	obj.usage()             print this 
+	obj.checkBounds()       checks that obj.start and obj.end is valid addresses. 
+	obj.getData()           copies the binary data between obj.start and obj.end to obj.buffer
+	obj.run()               the selected data is copied to the buffer in a binary format
+	obj.patch()             patch the IDB at obj.start with the data in the obj.buffer. 
+	obj.patch(d)            patch the IDB at obj.start with the argument data. 
+	obj.importb()           opens a file and saves the data in obj.buffer.
+	obj.export()            exports the data in obj.buffer to a save as file.
+	obj.l()                 print length of the buffer
+	'''
         
     def checkBounds(self):
         if self.start is BADADDR or self.end is BADADDR:
             self.status = False 
 
     def getData(self):
-        
+        '''get data betweeen start and end put them into object.buffer'''
         self.ogLen = self.end - self.start  
         try:
             for byte in GetManyBytes(self.start, self.ogLen):
@@ -35,19 +64,22 @@ class fwrapper():
         return
         
     def run(self):
+	'''basically main'''
         self.checkBounds()
         if self.status == False:
-            sys.stdout.write('ERROR: Please select valid data')
+            sys.stdout.write('ERROR: Please select valid data\n')
             return 
         self.getData()
          
-    def patch(self):
-        'patch idb with data in fwrapper.buffer'
+    def patch(self, temp = None):
+        '''patch idb with data in fwrapper.buffer'''
+	if temp != None:
+		self.buffer = temp
         for index, byte in enumerate(self.buffer):
              PatchByte(self.start+index, ord(byte))
              
     def importb(self):
-        'import file to save to buffer'
+        '''import file to save to buffer'''
         fileName = AskFile(0, "*.*", 'Import File')
         try:
             self.buffer = open(fileName, 'r').read()
@@ -55,9 +87,12 @@ class fwrapper():
             sys.stdout.write('ERROR: Cannot access file')
                
     def export(self):
-        'save the selected buffer to a file'
+        '''save the selected buffer to a file'''
         exportFile = AskFile(1, "*.*", 'Export Buffer')
         f = open(exportFile, 'wb')
         f.write(self.buffer)
         f.close()
-        
+       
+    def l(self):
+	print hex(len(self.buffer))
+    
